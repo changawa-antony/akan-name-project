c7caa6228b82ee71cb64224d09177ebad1a109ad
changawa-antony <antony.changawa@student.moringaschool.com>
2021-04-11 20:08:24 +0300

    Added js code for finding Akan name

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
+document.getElementById("submit-btn").addEventListener("click", formData);
 
-/* Arrays to store akan names
-
-var maleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
-var femaleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
-
-*/
-
-window.onload = function() {
-    var dataCollect = document.getElementById("submit-btn");
-    dataCollect.onclick = formDataValidation;
-
-};
-
-
-function formDataValidation(){
+function formData(){
 
     /* getting the values from the form */
 
     var dateValue = parseInt(document.getElementById("birthDate").value);
     var monthValue = parseInt(document.getElementById("birhtMonth").value);
     var yearValue = parseInt(document.getElementById("birthYear").value);
-    var maleValue = parseInt(document.getElementById("genderMale").value);
-    var femaleValue = parseInt(document.getElementById("genderFemale").value);
+    var genderValue = document.querySelector('input[type="radio"]:checked').value;
 
+    /*validating the date and month input*/
 
-    /*validating the data*/
-
-    if (dateValue <= 0 || dateValue > 31){  
-        document.getElementById("datespace").innerHTML="Enter a valid date";  
+    if (dateValue <= 0 || dateValue > 31&& monthValue <=0 || monthValue > 12){  
+        document.getElementById('display').innerHTML = "Date or Month is not valid";
+        return false;
     }
       else{
-        return true;
+
+        /* Getting last 2 digits of the year */
+        
+        var strYear = yearValue.toString();
+        var shortYear = strYear.substr(-2);
+        var yy = parseInt(shortYear);
+        
+        /*Getting century period */
+
+        var cc = Math.ceil(yearValue/100);
+
+        /* Getting day of birth */
+
+        var getday =(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(monthValue+1)/10)) + dateValue) % 7;
+        var day = parseInt(getday.toFixed(0));
+
+        /* Getting Akan name */
+
+        var maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+        if (genderValue == "female"){
+            var akanName = femaleAkan[day];
+        }
+        else{
+            var akanName = maleAkan[day];
+        }
+
+        document.getElementById('display').innerHTML = "Your Akan name is " + akanName;
+
+        document.getElementById("myForm").reset();
+
     };
 
-    /*validating month*/
-    if (monthValue <= 0 || monthValue > 12){  
-        document.getElementById("monthspace").innerHTML="Enter a valid date";  
-    }
-      else{
-        return true;
-    };
-
-
+};
    
-    
 
     
-
-}
 
 
 
