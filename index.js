d0dce4ff3efd8439137f91ba72061b91c7893379
changawa-antony <antony.changawa@student.moringaschool.com>
2021-04-11 07:43:37 +0300

    Added akan name arrays and a function for form input collection

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* Arrays to store akan names */
+
+var maleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+var femaleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+
+window.onload = function() {
+    var dataCollect = document.getElementById("submit-btn");
+    dataCollect.onclick = formDataValidation;
+
+};
+
+
+function formDataValidation(){}
