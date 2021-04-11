cd8594f53295674930974126f877d0e1f7be0f58
changawa-antony <antony.changawa@student.moringaschool.com>
2021-04-11 09:52:30 +0300

    Added form vallidation code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-/* Arrays to store akan names */
+/* Arrays to store akan names
 
 var maleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
 var femaleAkan = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+
+*/
 
 window.onload = function() {
     var dataCollect = document.getElementById("submit-btn");
@@ -11,4 +13,41 @@ window.onload = function() {
 };
 
 
-function formDataValidation(){}
+function formDataValidation(){
+
+    /* getting the values from the form */
+
+    var dateValue = parseInt(document.getElementById("birthDate").value);
+    var monthValue = parseInt(document.getElementById("birhtMonth").value);
+    var yearValue = parseInt(document.getElementById("birthYear").value);
+    var maleValue = parseInt(document.getElementById("genderMale").value);
+    var femaleValue = parseInt(document.getElementById("genderFemale").value);
+
+
+    /*validating the data*/
+
+    if (dateValue <= 0 || dateValue > 31){  
+        document.getElementById("datespace").innerHTML="Enter a valid date";  
+    }
+      else{
+        return true;
+    };
+
+    /*validating month*/
+    if (monthValue <= 0 || monthValue > 12){  
+        document.getElementById("monthspace").innerHTML="Enter a valid date";  
+    }
+      else{
+        return true;
+    };
+
+
+   
+    
+
+    
+
+}
+
+
+
